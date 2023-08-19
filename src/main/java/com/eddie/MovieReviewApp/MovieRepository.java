66f4f23eefd187a8f2797b0eabbcfb29f9babc4a
eddie-coding <adityabahuguna007@gmail.com>
2023-08-19 13:49:07 +0530

    Fetch Movies from MongoDB to backend

---
+++ src/main/java/com/eddie/MovieReviewApp/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.eddie.MovieReviewApp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
